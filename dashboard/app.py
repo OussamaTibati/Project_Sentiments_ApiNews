dced3504c13bc0798940396047109ee4ff94d0bf
OussamaTib <tibatioussama@gmail.com>
2025-05-13 21:11:03 +0100

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,10 +1,10 @@
 import dash
-import os
 from dash import dcc, html
 import plotly.express as px
 import pandas as pd
+import os
 
-# Exemple de données à remplacer par les vraies via fichier/parquet/BDD
+# Exemple de données
 df = pd.DataFrame({
     "Article": ["News 1", "News 2"],
     "Sentiment": [0.2, -0.1]
@@ -19,4 +19,4 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
